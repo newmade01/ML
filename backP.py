10427121b6d780f36e581b3861c5561a0e968be0
JaKyung <57945595+newmade01@users.noreply.github.com>
2022-02-18 17:01:20 +0900

    backPass

--- backP.py
+++ backP.py
@@ -1,1 +1,11 @@
 import tensorflow as tf
+
+def training(self, x, y):
+    m = len(x) #image num
+
+    with tf.GradientTap() as tape:
+        z = self.forpass(x)#정방향 계산
+
+        #loss 값 logit을 대상으로 손실값을 계산
+        loss = tf.nn. softmax_cross_entropy_with_logits(y, z)
+        loss = tf.reduce_mean(loss) #손실 평균화
