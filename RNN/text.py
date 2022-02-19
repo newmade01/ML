b299c49d06b68940507d149d8ec5ea5100a2b1ed
JaKyung <57945595+newmade01@users.noreply.github.com>
2022-02-19 18:15:08 +0900

    data Load

--- RNN/text.py
+++ RNN/text.py
@@ -1,1 +1,7 @@
 import numpy as np
+from tensorflow.keras.datasets import imdb
+
+#data load, get
+(x_train_all, y_train_all), (x_test, y_test) =imdb.load_data(skip_top=20, num_words=100)
+
+print(x_train_all.shape, y_train_all.shape)
