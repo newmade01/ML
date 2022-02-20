890438ee057ad4fe2a653de52ea21be9766b7046
JaKyung <57945595+newmade01@users.noreply.github.com>
2022-02-20 14:16:06 +0900

    정방향 계산

--- RNN/text.py
+++ RNN/text.py
@@ -17,3 +17,14 @@ x_val_sequence = sequence.pad_sequences(x_val, maxlen=maxlen)
 from tensorflow.keras.utils import to_categorical
 x_train_onehot = to_categorical(x_train_sequence)
 x_val_onehot = to_categorical(x_val_sequence)
+
+#배치 차원& 타임스텝 차원 변경: 코드를 위해 편의상
+seq = np.swapaxes(x, 0, 1)
+
+#정방향 계산 #np.dot: 행렬의 곱
+for x in seq:
+    z1 = np.dot(x, self.wx1) + np.dot(self.h[-1], self.w1h) +self.b1
+    h = np.tanh(x) #활성화 함수
+    self.h.append(h) #이전의 h값을 계속 사용하기 떄문에 버리면 안됨
+    z2 = np.dot(h, self.w2)  + self.b2 #출력층
+return z2
