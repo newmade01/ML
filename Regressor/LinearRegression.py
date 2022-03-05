463471b91c696d5cd79fccf2f1f888c5f2608d51
JaKyung <57945595+newmade01@users.noreply.github.com>
2022-03-05 23:12:44 +0900

    선형회귀- 다항회귀

--- Regressor/LinearRegression.py
+++ Regressor/LinearRegression.py
@@ -3,6 +3,8 @@
 
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 lr=LinearRegression()
 lr.fit(train_input,train_target)
@@ -22,3 +24,22 @@ plt.show()
 
 lr.score(train_input, train_target)
 lr.score(test_input, test_target)
+
+
+#다항회귀 추가 X^2, X의 값을 열로 붙침
+train_poly = np.column_stack((train_input **2, train_input))
+test_poly = np.column_stack((test_input**2, test_input))
+lr.fit(train_poly, train_target)
+lr.predict([[50**2, 50]]) #임의의 값 예
+print(lr.coef_, lr.intercept_)
+#구간별 직선 임의 선정 15~49까지
+point = np.arrange(15, 50)
+plt.scatter(train_poly, train_target)#훈련 세트 산점도
+#2차 방정식 그래프 그리기
+plt.plot(point, lr.coef_[0]*point**2+lr.coef[1]*point+lr.intercept_)
+#50센치 농어 데이터 그리기
+plt.scatter([50],[1574],marker='^')
+plt.show()
+lr.score(train_poly, train_target)
+lr.score(test_poly, test_target)
+
