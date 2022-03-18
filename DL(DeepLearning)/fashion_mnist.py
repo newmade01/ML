d628428880fb2a06e1b053eb9921582506020805
JaKyung <57945595+newmade01@users.noreply.github.com>
2022-03-19 01:37:52 +0900

    로지스틱회귀

--- DL(DeepLearning)/fashion_mnist.py
+++ DL(DeepLearning)/fashion_mnist.py
@@ -6,7 +6,7 @@ print(tf.config.list_physical_devices())
 print(tf.__version__)
 
 from tensorflow import keras
-
+import numpy as np
 (train_input, train_target), (test_input, test_target) = keras.datasets.fashion_mnist.load_data()
 
 print(train_input.shape, train_target.shape)    #6만개 샘플,28*28픽셀
@@ -19,4 +19,18 @@ for i in range(10):
     axs[i].imshow(train_input[i], cmap='gray_r')
     axs[i].axis('off')
 
-plt.show()
+#plt.show()
+
+print([train_target[i] for i in range(10)])
+print(np.unique(train_target, return_counts=True))
+
+#로지스틱 회귀
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import cross_validate
+train_scaled = train_input / 255.0
+train_scaled = train_scaled.reshape(-1, 28*28) #3차원 ->1차원 (샘플수, 높이*너비)
+print(train_scaled.shape)   #(샘플수, 2차원*3차원값)
+sc = SGDClassifier(loss='log', max_iter=5, random_state=42) #max_iter =  epoch 반복횟수 #2진분류: 손실함수 / 다중분류: 10개의 이진분류 사용
+scores = cross_validate(sc, train_scaled, train_target, n_jobs=-1) #기본값 5fold
+print(np.mean(scores['test_score']))
+
