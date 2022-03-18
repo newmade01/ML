a5b99fee4944f72a56ac2ef1d07539f207c23b1d
JaKyung <57945595+newmade01@users.noreply.github.com>
2022-03-19 02:22:03 +0900

    케라스 모델

--- DL(DeepLearning)/Keras.py
+++ DL(DeepLearning)/Keras.py
@@ -25,3 +25,10 @@ print(val_scaled.shape, val_target.shape)
 #Dense:  keras의 기본층 ,(밀집층 = 완전연결층 =fully connected layer)
 dense = keras.layers.Dense(10, activation='softmax', input_shape=(784,)) #(출력층 10개의 뉴런= 클래스의 갯수, 활성화함수, 샘플의 크기)
 model = keras.Sequential(dense) #Sequential: 순서대로 넣음
+
+#모델 설정
+model.complie(loss='sparse_categorical_crossentropy', metrics='accuracy')   #손실함: 1.이진분류: loss='binary_crossentropy', 2.다중분류: loss='categorical_crossentropy', sparse: 정수 타깃의 원핫 인코딩
+print(train_target[:10])
+
+model.fit(train_scaled, train_target, epochs=5)
+model.evaluate(val_scaled, val_target)
