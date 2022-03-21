1fc36550e2a3c4009baa0a05f25c19b86fe37c17
JaKyung <57945595+newmade01@users.noreply.github.com>
2022-03-22 00:14:49 +0900

    CNN 이미지분류

--- DL(DeepLearning)/CNN_합성곱신경망/CNN_시각화.py
+++ DL(DeepLearning)/CNN_합성곱신경망/CNN_시각화.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 conv = model.layers[0]
 print(conv.weghts[0].shape, conv.weghts[1].shape)   #가중치, 절편    (픽셀, 필셀, 채널, 필터)
 
-conv_weights = conv.weghts[0].numpy()
+conv_weights = conv.weghts[0].numpy()   #일반적으로 가중치는 균등분포
 plt.hist(conv_weights.reshape(-1, 1))    #1차원 열 백터로 펼침, 히스토그램 그래프
 plt.xlabel('weight')
 plt.ylabel('count')
