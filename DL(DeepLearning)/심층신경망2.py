b644f0ede7a1d21b99ed158317fb8c2ea9acd3d8
JaKyung <57945595+newmade01@users.noreply.github.com>
2022-03-21 19:07:21 +0900

    심층신경망

--- DL(DeepLearning)/심층신경망2.py
+++ DL(DeepLearning)/심층신경망2.py
@@ -32,3 +32,16 @@ model.load_weights('w.h5')  #불러옴
 model.save('model_whole.h5')    #모델구조&가중치 모두 저장
 model = keras.models.load_model('model_whole.h5')   # 모델구조&가중치 모두 불러옴
 
+#샘플마다 가장 큰 확률 값의 index 가져옴
+val_labels = np.argmax(model.predic(val_scaled), axis=-1) #예측  #axis =-1 은 axis=1 값과 동일 열..
+print(np.mean(val_labels == val_target))    #True의 값들 평균이 나옴
+
+###콜백 : 진행도중 지정한 작업을 시행
+checkpoint_cb = keras.callbacks.ModelCheckpoitnt('best-model.h5')    #가장 낮은 손실값을 저장
+model.fit(train_scaled, train_target, epochs=20, verbose=0, validation_data=(val_scaled, val_target), callbacks=[checkpoint_cb])
+
+model=  keras.models.load_model('best-model.h5')    #사용시 불러옴
+
+###조기종료: 검증세트의 손실이 증가시
+checkpoint_cb = keras.callbacks.ModelCheckpoitnt('best-models.h5')
+early_stopping_cb = keras.callbacks
