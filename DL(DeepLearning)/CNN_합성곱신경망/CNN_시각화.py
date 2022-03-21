9187eabbca7e3178384dc08e6ecaa7dfb1220aab
JaKyung <57945595+newmade01@users.noreply.github.com>
2022-03-22 00:11:46 +0900

    CNN 이미지분류

---
+++ DL(DeepLearning)/CNN_합성곱신경망/CNN_시각화.py
@@ -0,0 +1,12 @@
+#functional API
+import matplotlib.pyplot as plt
+
+conv = model.layers[0]
+print(conv.weghts[0].shape, conv.weghts[1].shape)   #가중치, 절편    (픽셀, 필셀, 채널, 필터)
+
+conv_weights = conv.weghts[0].numpy()
+plt.hist(conv_weights.reshape(-1, 1))    #1차원 열 백터로 펼침, 히스토그램 그래프
+plt.xlabel('weight')
+plt.ylabel('count')
+plt.show()
+
