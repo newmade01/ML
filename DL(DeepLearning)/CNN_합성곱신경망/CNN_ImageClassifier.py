e8727213cec1f7a330ff36678b94d2a54c13207c
JaKyung <57945595+newmade01@users.noreply.github.com>
2022-03-21 23:41:57 +0900

    CNN 이미지분류

--- DL(DeepLearning)/CNN_합성곱신경망/CNN_ImageClassifier.py
+++ DL(DeepLearning)/CNN_합성곱신경망/CNN_ImageClassifier.py
@@ -12,4 +12,11 @@ train_scaeld, val_scaled, train_target, val_target = train_test_split(train_scae
 model = keras.Sequential()
 model.add(keras.layers.Conv2D(32, kernel_size=3, activation='relu', padding='same', input_shape=(28, 28, 1)))   #배치차원은 써주지 않음
 model.add(keras.layers.MaxPooling2D(2)) #최대풀링(2*2) 정방향으로 풀링, 반으로 축소
-###두번째 합성곱층+완전 연결층(Dense)
+###두번째 합성곱층 추가+완전 연결층(Dense)
+model.add(keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(keras.layers.MaxPooling2D(2)) #(2*2) pooling
+#분류: 1차원 배열로 총 펼침 입력값
+model.add(keras.layers.Flatten())   #입력배열을 1차원으로 펼침
+model.add(keras.layers.Densc(100, activation='relu'))   #100개의 뉴런 은닉층
+model.add(keras.layers.Dropout(0.4))
+model.add(keras.layers.Dense(10, activation='softmax')) #10개의 뉴런 출력층
