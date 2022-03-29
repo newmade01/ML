c590fbe3f6b05b3823aeef20c9e2880d0314e8f6
JaKyung <57945595+newmade01@users.noreply.github.com>
2022-03-29 21:36:35 +0900

    yolo_train

--- DL(DeepLearning)/CNN_합성곱신경망/Yolo/YOLOv5_train.py
+++ DL(DeepLearning)/CNN_합성곱신경망/Yolo/YOLOv5_train.py
@@ -1,11 +1,11 @@
-#데이터셋정리
+###데이터셋정리
 from glob import glob
 
 train_img_list = glob('content/yolov5/pothole/train/images/*.jpg')
 test_img_list = glob('content/yolov5/pothole/test/images/*.jpg')
 valid_img_list = glob('content/yolov5/pothole/valid/images/*.jpg')
 
-# text파일로 이미지 리스트 파일 저장
+### text파일로 이미지 리스트 파일 저장
 import yaml
 
 with open('content/yolov5/pothole/train.txt', 'w') as f:
@@ -35,4 +35,15 @@ nc: 1
 names: ['pothole']
 '''
 
-#모델 구성
+###모델 구성
+
+import yaml
+with open('/content/yolov5/pothole/data.yaml', 'r') as stream:
+  num_classes = str(yaml.safe_load(stream)['nc']) #number classes
+
+#%cat /content/yolov5/models/yolov5s.yaml
+
+#%%writetemplate /content/yolov5/models/custom_yolov5s.yaml #새로운 custom 버전을 변경
+
+###학습
+
