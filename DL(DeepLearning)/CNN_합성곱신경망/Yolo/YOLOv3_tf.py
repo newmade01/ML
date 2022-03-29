585e6d91163af000076463821afe9a60720d6a90
JaKyung <57945595+newmade01@users.noreply.github.com>
2022-03-29 20:24:38 +0900

    yolo

--- DL(DeepLearning)/CNN_합성곱신경망/Yolo/YOLOv3_tf.py
+++ DL(DeepLearning)/CNN_합성곱신경망/Yolo/YOLOv3_tf.py
@@ -2,13 +2,13 @@ def detector():
   ima_raw = tf.image.decode_image(open(FLAGS.image, 'rb').read(), channels=3)
 
   img = tf.expand_dims(ima_raw, 0)  #디멘젼 형태로 변환
-  img = transform_images(img, FLAGS.size)
+  img = transform_images(img, FLAGS.size) #사이즈 고정
 
   t1 = time.time()
   boxs, scores, classes, nums = yolo(img)
   t2 = time.time() 
-  #t2-t1을 빼면 실제 yolo가 실행된 시간
-  print('time: {}'.format(t2-t1))
+
+  print('time: {}'.format(t2-t1)) #t2-t1을 빼면 실제 yolo가 실행된 시간
 
   for i in range(num[0]):
     .print('{}, {}, {}'.format(class_names[int(classes[0][i])],
