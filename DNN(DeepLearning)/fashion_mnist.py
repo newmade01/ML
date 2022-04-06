e11981c5b05c48b4b8da32678012c22d4621969f
JaKyung <57945595+newmade01@users.noreply.github.com>
2022-04-06 17:15:41 +0900

    Update fashion_mnist.py

--- DNN(DeepLearning)/fashion_mnist.py
+++ DNN(DeepLearning)/fashion_mnist.py
@@ -12,7 +12,7 @@ print(train_input.shape, train_target.shape)    #6만개 샘플,28*28픽셀
 print(test_input.shape, test_target.shape)
 
 #fig: 전체 그래프를 저장하는 변수, axes: 그 안의 subplots을 저장하는 변수
-fig, axs = plt.subplots(1, 10, figsize=(10, 10))
+fig, axs = plt.subplots(1, 10, figsize=(10, 10)) #nrows=1, ncols=10, (index)
 for i in range(10):
     axs[i].imshow(train_input[i], cmap='gray_r')
     axs[i].axis('off')
